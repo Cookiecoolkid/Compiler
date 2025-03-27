27e87128743a76353355ee519f4c54bf4841900a
Cookiecoolkid <1766462664@qq.com>
2025-03-27 17:12:29 +0800

    add proj1 test

---
+++ proj1/Test/try3.c
@@ -0,0 +1,24 @@
+int main() {
+int a = 1e;
+int b = 1;
+}
+
+/*
+int main(){
+	float a;
+	if(a==0) 
+	i=1;
+	else i=2
+	return 5+-1;
+}
+
+*/
+
+/*
+int main(){
+	float a;
+	if(a==0) i=1;
+	else i=2
+	return 0;
+}
+*/
