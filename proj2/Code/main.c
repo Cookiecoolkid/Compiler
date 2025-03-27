116b9b2c5fd75e4c62e3cf175fe50667b60b44f1
Cookiecoolkid <1766462664@qq.com>
2025-03-27 17:14:18 +0800

    init proj2

---
+++ proj2/Code/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "syntax.tab.h"
+extern int yylex();
+// extern int lines, words, chars;
+extern FILE *yyin;
+
+extern int has_error;
+extern struct Node* root;
+extern void print_tree(struct Node *node, int indent);
+extern void free_tree(struct Node *node);
+
+int main(int argc, char **argv) {
+  if (argc <= 1) {
+      fprintf(stderr, "Usage: %s <input file>\n", argv[0]);
+      return 1;
+  }
+
+  if (!(yyin = fopen(argv[1], "r"))) {
+      perror(argv[1]);
+      return 1;
+  }
+
+  yyparse();
+
+  if (!has_error) {
+      print_tree(root, 0);
+  }
+
+  free_tree(root);
+  return 0;
+}
---
+++ proj2/Code/node.h
@@ -0,0 +1,30 @@
+#pragma once
+
+typedef enum {
+    VALUE_INT,
+    VALUE_FLOAT,
+    VALUE_STRING,
+    VALUE_OTHER
+} ValueType;
+
+typedef struct {
+    ValueType type;
+    union {
+        int int_val;
+        float float_val;
+        char *str_val;
+    } value;
+} NodeValue;
+
+typedef struct Node {
+    char *name;
+    int line;
+    NodeValue value;
+    struct Node *next;
+    struct Node *child;
+} Node;
+
+Node *create_node(const char *name, int line, ValueType type, ...);
+void link_nodes(Node *parent, ...);
+void print_tree(Node *node, int indent);
+void free_tree(Node *node);
