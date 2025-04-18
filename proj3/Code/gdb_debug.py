ad4f1e888acd920742c5a4699261f2b2862e967f
Cookiecoolkid <1766462664@qq.com>
2025-04-18 17:17:35 +0800

    add gdb-py-script Usage: gdb ./parser -x gdb_debug.py

---
+++ proj3/Code/gdb_debug.py
@@ -0,0 +1,15 @@
+import gdb
+
+def setup_debug():
+    # 设置断点并运行程序
+    # gdb.execute('b /home/cookiecoolkid/Compiler/proj3/Code/translate.c:589')
+    gdb.execute('run ../Test/test1.cmm ../Output/test1.ir')
+    # gdb.execute('layout src')
+    gdb.execute('set pagination off')
+
+# 注册调试设置函数
+gdb.execute('set confirm off')  # 关闭确认提示
+setup_debug()
+
+# 继续执行，进入调试模式
+# gdb.execute('continue')
