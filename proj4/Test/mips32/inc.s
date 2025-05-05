e6e7fab09ba61fdd10bb3dcaf525b86c5015dd96
Cookiecoolkid <1766462664@qq.com>
2025-05-05 18:50:57 +0800

    upd tests

---
+++ proj4/Test/mips32/inc.s
@@ -0,0 +1,77 @@
+	.file	1 "inc.c"
+	.section .mdebug.abi32
+	.previous
+	.nan	legacy
+	.module	fp=xx
+	.module	nooddspreg
+	.abicalls
+	.text
+	.align	2
+	.globl	inc
+	.set	nomips16
+	.set	nomicromips
+	.ent	inc
+	.type	inc, @function
+inc:
+	.frame	$fp,16,$31		# vars= 8, regs= 1/0, args= 0, gp= 0
+	.mask	0x40000000,-4
+	.fmask	0x00000000,0
+	.set	noreorder
+	.set	nomacro
+	addiu	$sp,$sp,-16
+	sw	$fp,12($sp)
+	move	$fp,$sp
+	sw	$4,16($fp)
+	lw	$2,16($fp)
+	addiu	$2,$2,1
+	sw	$2,4($fp)
+	lw	$2,4($fp)
+	move	$sp,$fp
+	lw	$fp,12($sp)
+	addiu	$sp,$sp,16
+	jr	$31
+	nop
+
+	.set	macro
+	.set	reorder
+	.end	inc
+	.size	inc, .-inc
+	.align	2
+	.globl	main
+	.set	nomips16
+	.set	nomicromips
+	.ent	main
+	.type	main, @function
+main:
+	.frame	$fp,40,$31		# vars= 8, regs= 2/0, args= 16, gp= 8
+	.mask	0xc0000000,-4
+	.fmask	0x00000000,0
+	.set	noreorder
+	.set	nomacro
+	addiu	$sp,$sp,-40
+	sw	$31,36($sp)
+	sw	$fp,32($sp)
+	move	$fp,$sp
+	li	$2,4			# 0x4
+	sw	$2,24($fp)
+	lw	$4,24($fp)
+	.option	pic0
+	jal	inc
+	nop
+
+	.option	pic2
+	sw	$2,28($fp)
+	move	$2,$0
+	move	$sp,$fp
+	lw	$31,36($sp)
+	lw	$fp,32($sp)
+	addiu	$sp,$sp,40
+	jr	$31
+	nop
+
+	.set	macro
+	.set	reorder
+	.end	main
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 10.3.0-1ubuntu1) 10.3.0"
+	.section	.note.GNU-stack,"",@progbits
--- proj4/Test/tmp/inc.s
+++ proj4/Test/tmp/inc.s
@@ -23,25 +23,90 @@ move $v0, $0
 jr $ra
 
 inc:
-lw $t0, temp0_addr
-lw $t1, a__addr
-addi $t0, $t1, 1
-lw $t2, b__addr
-move $t2, $t0
+subu $sp, $sp, 40
+sw $ra, 36($sp)
+sw $fp, 32($sp)
+addi $fp, $sp, 40
+sw $s0, 28($sp)
+sw $s1, 24($sp)
+sw $s2, 20($sp)
+sw $s3, 16($sp)
+sw $s4, 12($sp)
+sw $s5, 8($sp)
+sw $s6, 4($sp)
+sw $s7, 0($sp)
+lw $t0, 0($fp)
+lw $t1, 0($fp)
+li $t2, 1
+add $t3, $t1, $t2
+lw $t2, 8($fp)
+move $t2, $t3
+lw $s7, 0($sp)
+lw $s6, 4($sp)
+lw $s5, 8($sp)
+lw $s4, 12($sp)
+lw $s3, 16($sp)
+lw $s2, 20($sp)
+lw $s1, 24($sp)
+lw $s0, 28($sp)
+lw $ra, 36($sp)
+lw $fp, 32($sp)
 move $v0, $t2
+addi $sp, $sp, 40
 jr $ra
 
 main:
-lw $t3, #2_addr
-mul $t4, $t3, $t3
-lw $t5, lcVar__addr
-move $t5, $t4
+subu $sp, $sp, 52
+sw $ra, 48($sp)
+sw $fp, 44($sp)
+addi $fp, $sp, 52
+sw $s0, 40($sp)
+sw $s1, 36($sp)
+sw $s2, 32($sp)
+sw $s3, 28($sp)
+sw $s4, 24($sp)
+sw $s5, 20($sp)
+sw $s6, 16($sp)
+sw $s7, 12($sp)
+li $t4, 2
+li $t5, 2
+mul $t6, $t4, $t5
+lw $t4, 16($fp)
+move $t4, $t6
+sw $t4, 0($sp)
+sw $t1, 0($sp)
+sw $t2, 8($sp)
+sw $t3, 4($sp)
+sw $t4, 16($sp)
+sw $t6, 12($sp)
 jal inc
-lw $t6, temp2_addr
-move $t6, $v0
-lw $t7, rcVar__addr
-move $t7, $t6
-lw $s0, #0_addr
+lw $t1, 0($sp)
+lw $t2, 8($sp)
+lw $t3, 4($sp)
+lw $t4, 16($sp)
+lw $t6, 12($sp)
+lw $t5, 20($fp)
+move $t5, $v0
+lw $t7, 24($fp)
+move $t7, $t5
+move $a0, $t7
+subu $sp, $sp, 4
+sw $ra, 0($sp)
+jal write
+lw $ra, 0($sp)
+addi $sp, $sp, 4
+lw $s7, 12($sp)
+lw $s6, 16($sp)
+lw $s5, 20($sp)
+lw $s4, 24($sp)
+lw $s3, 28($sp)
+lw $s2, 32($sp)
+lw $s1, 36($sp)
+lw $s0, 40($sp)
+lw $ra, 48($sp)
+lw $fp, 44($sp)
+lw $s0, 28($fp)
 move $v0, $s0
+addi $sp, $sp, 52
 jr $ra
 
